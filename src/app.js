45ee0169cb74e1e806a0193d0e3fbf102058d3cb
Jorge Carrion <jorge_28cs@hotmail.com>
2020-09-16 14:38:20 -0500

    First commit

---
+++ src/app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+// Init modules
+const app = express();
+
+// Settings
+app.set('port', 3000);
+app.set('views', path.resolve(__dirname, 'views'));
+
+// Middlewares
+app.use(express.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+// Routes
+app.use(require('./routes/index.js'));
+
+
+// 404 handler
+app.use((req, res) => {
+  res.status(404).render('404');
+});
+
+// Init Server
+const server = app.listen(app.get('port'), () => console.log('Server on port', app.get('port')))
