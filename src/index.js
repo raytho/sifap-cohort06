ca8423aa7ca7c714281119e1634b68d97a5f42c9
Jorge Carrion <jorge_28cs@hotmail.com>
2020-09-27 00:55:31 -0500

    Cors added

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@
 // Modules
 const express = require("express");
 const helmet = require("helmet");
-// const config = require("./config");
 const notFoundHandler = require("./utils/middleware/notFoundHandler");
 const authApiRouter = require("./routes/api/auth");
 const rolesApiRouter = require("./routes/api/roles");
-const home = require("./routes/views/home");
+const home = require("./routes/views/home"); 
 const userViewRouter = require("./routes/views/user");
 const superAdminRouter = require("./routes/views/superAdmin");
 require("dotenv").config();
+const cors = require("cors");
 
 // App
 const app = express();
@@ -21,6 +21,8 @@ const _port = process.env.PORT || 3000;
 // Middlewares
 app.use(express.json());
 app.use(helmet());
+app.user(cosr());
+
 
 // Routes
 app.use("/", home);
