7b06e684a68024d1b0aa73f70f3e0b3612816ec9
Jorge Carrion <jorge_28cs@hotmail.com>
2020-10-15 18:59:56 -0500

    message login unauthorized

--- src/routes/api/auth.js
+++ src/routes/api/auth.js
@@ -27,9 +27,10 @@ function authApi(app) {
   router.post("/sign-in", async (req, res, next) => {
     passport.authenticate("basic", async (error, user) => {
       try {
-        console.log(user);
         if (error || !user) {
-          next(boom.unauthorized());
+          res.status(500).json({
+            message: "No autorizado",
+          });
         } else {
           if (user.twoFactorActive) {
             generateTempToken(req, res, next, user);
