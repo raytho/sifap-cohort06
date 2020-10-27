f34d3f0de9b93bd71a90d914154d4a55f2dd1a3a
Jorge Carrion <jorge_28cs@hotmail.com>
2020-10-27 06:49:53 -0600

    deleted fiscalId on register

--- src/utils/schemas/users.js
+++ src/utils/schemas/users.js
@@ -4,7 +4,6 @@ const userIdSchema = joi.string().regex(/^[0-9a-fA-F]{24}$/);
 
 const userSchema = {
   firstName: joi.string().required().required(),
-  fiscalId: joi.string().required().required(),
   email: joi.string().email().required(),
   password: joi.string().required().required(),
   passwordVerify: joi.string(),
