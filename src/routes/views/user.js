18ed98d932fadd526c30d8a83d0e5b90df298d1a
Jorge Carrion <jorge_28cs@hotmail.com>
2020-11-07 08:29:18 -0600

    qr-code fix

--- src/routes/views/user.js
+++ src/routes/views/user.js
@@ -51,6 +51,26 @@ function userView(app) {
     })(req, res, next);
   });
 
+  router.get("/settings/send-qr", async (req, res, next) => {
+    passport.authenticate("jwt", { session: false }, async (error, user) => {
+      try {
+        if (error || !user) {
+          res.status(500).json({
+            message: "No autorizado",
+          });
+        } else {
+          twoFactorAuth.generateQr().then((qrCode) => {
+            res.status(200).json({
+              message: qrCode,
+            });
+          });
+        }
+      } catch (error) {
+        next(error);
+      }
+    })(req, res, next);
+  });
+
   router.post("/tax-receipt", async (req, res, next) => {
     validationHandler(fiscalDataSchema),
     passport.authenticate("jwt", { session: false }, async (error, user) => {
