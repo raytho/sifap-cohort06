0a978b6e17468f9316a43f366f62f4ebe085f9ca
Jorge Carrion <jorge_28cs@hotmail.com>
2020-11-07 13:35:05 -0600

    Removed dummy endpoints

--- src/routes/api/auth.js
+++ src/routes/api/auth.js
@@ -225,21 +225,12 @@ function authApi(app) {
     })(req, res, next);
   });
 
-  router.get("/token", async (req, res) => {
-    const secret = config.twoFactorSecret;
-    const token = twoFactorAuth.generateTotpToken(secret);
-    res.status(200).json({
-      message: token,
-    });
-  });
-
   router.post("/forgot", async (req, res, next) => {
     const email = req.body.email;
     if (email) {
       const user = await usersService.getUserByMail({ email });
 
       if (!user) {
-        // Mejor sería next(boom.unauthorized()); para no enviar indicios del mail
         res.status(404).json({
           error: "No account with that email address exists.",
           data: null,
@@ -347,8 +338,7 @@ function authApi(app) {
   });
 
   router.get("/logout", function (req, res, next) {
-    // eslint-disable-next-line no-unused-vars
-    passport.authenticate("jwt", { session: false }, (error, user) => {
+    passport.authenticate("jwt", { session: false }, () => {
       req.logOut();
       return res.redirect("/");
     })(req, res, next);
