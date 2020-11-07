e2d6b69212a130374de9944d4f96a91efff95575
Raymundo Solis <pepperray01@gmail.com>
2020-11-07 13:59:14 -0600

    fixing edituserRole typo2

--- src/routes/views/superAdmin.js
+++ src/routes/views/superAdmin.js
@@ -256,6 +256,7 @@ const inviteNewUser = (app) => {
         const data = req.body;
         const userService = new usersService();
         try {
+          cosnole.log("Request data => ", data);
           if (data.role) {
             const updatedUser = await userService.updateRolByUserId(id, data);
             if (updatedUser) {
